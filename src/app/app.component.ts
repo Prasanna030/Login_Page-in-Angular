08edddd5e35d90eb6b88a9c61478fa3040e849f0
Prasanna <s.prasann03@gmail.com>
2023-06-01 10:38:17 +0530

    Updated code for login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loginpage';
+  username:string='';
+  password:string='';
+  
+  Verify(){
+    if(this.username=="Admin"&& this.password=="Admin"){
+      console.log('Welcome')
+      alert("Welcome Back")
+    }
+    else{
+      console.log("Invalid Username/Password")
+      alert("Invalid Username or Password, Please try again")
+    }
+  }
+
 }
